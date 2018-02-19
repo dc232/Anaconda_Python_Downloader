51358f8800d6e377f7df92a0ede82e1c9b916d35
DC <danny_chauhan@hotmail.com>
2018-02-19 13:23:57 +0000

    added sourcing of bashrc

--- Anaconda_Python_Downloader.sh
+++ Anaconda_Python_Downloader.sh
@@ -16,6 +16,8 @@ ANACONDA_VERSION="3-5.1.0"
 if [[ -d "$LOCAL" && "$ANACONDA_DONLOAD_CHECK" ]];  then
 echo "Directory exists prepairing to install Anaconda"
 bash $LOCAL/Anaconda$ANACONDA_VERSION-Linux-x86_64.sh
+echo "Sourcing content from bashrc"
+source ~/.bashrc
 elif [ "$LOCAL" ]; then 
 echo "making directory $LOCAL"
 sleep 1
@@ -27,6 +29,7 @@ echo "checking the sha256 checksum"
 sha256sum $LOCAL/Anaconda$ANACONDA_VERSION-Linux-x86_64.sh
 echo "sourcing Anaconda$ANACONDA_VERSION-Linux-x86_64 install script"
 bash $LOCAL/Anaconda$ANACONDA_VERSION-Linux-x86_64.sh
+source ~/.bashrc
 fi
 
 
